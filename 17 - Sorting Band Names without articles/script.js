23c5ad068dbc503044fb32f3345504f6198a5b36
tjgillweb <tjgillweb@gmail.com>
2020-02-25 16:17:06 +0530

    Update script with sorting code

---
+++ 17 - Sorting Band Names without articles/script.js
@@ -0,0 +1,10 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function strip(bandName) {
+    return bandName.replace(/^(a |an |the)/i, '').trim(); //case insensitive
+}
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector('#bands').innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
+
+console.log(sortedBands);
